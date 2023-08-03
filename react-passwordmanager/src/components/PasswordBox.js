5cb7370abe3db8d188eb08bcefc5496c856067f3
Abhinav Lal Shrestha <abhinavstha849@Abhinavs-Mini.attlocal.net>
2023-08-03 03:12:08 -0500

    Design fixed

--- react-passwordmanager/src/components/PasswordBox.js
+++ react-passwordmanager/src/components/PasswordBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Axios from "axios";
+import "./PasswordBox.css";
 
 function PasswordBox(props) {
   const { passwordList, decryptedPasswords, toggleDecryptedPassword, decryptPassword, onDelete } = props;
@@ -12,23 +13,42 @@ function PasswordBox(props) {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
+    <div className="column">
       {passwordList.map((val) => (
-        <div key={val.id} style={{ margin: "10px", padding: "10px", border: "1px solid black" }}>
-          <h3>Username: {val.account}</h3>
-          {decryptedPasswords[val.id] ? <p>Password: {decryptedPasswords[val.id]}</p> : <p>Password: *****</p>}
-          <p>Website: {val.Website}</p>
-          <button
-            onClick={() => {
-              if (!decryptedPasswords[val.id]) {
-                decryptPassword({ password: val.password, iv: val.iv, id: val.id });
-              }
-              toggleDecryptedPassword(val.id);
-            }}
-          >
-            {decryptedPasswords[val.id] ? "Hide Password" : "Show Password"}
-          </button>
-          <button onClick={() => handleDelete(val.id)}>Delete</button>
+        <div key={val.id} className="password">
+          <div className="row">
+            <span className="label">Username:</span>
+            <span className="value">{val.account}</span>
+          </div>
+          {decryptedPasswords[val.id] ? (
+            <div className="row">
+              <span className="label">Password:</span>
+              <span className="value">{decryptedPasswords[val.id]}</span>
+            </div>
+          ) : (
+            <div className="row">
+              <span className="label">Password:</span>
+              <span className="value">*****</span>
+            </div>
+          )}
+          <div className="row">
+            <span className="label">Website:</span>
+            <span className="value">{val.Website}</span>
+          </div>
+          <div className="row">
+            <button
+              className="btn"
+              onClick={() => {
+                if (!decryptedPasswords[val.id]) {
+                  decryptPassword({ password: val.password, iv: val.iv, id: val.id });
+                }
+                toggleDecryptedPassword(val.id);
+              }}
+            >
+              {decryptedPasswords[val.id] ? "Hide Password" : "Show Password"}
+            </button>
+            <button className="btn--delete-btn" onClick={() => handleDelete(val.id)}>Delete</button>
+          </div>
         </div>
       ))}
     </div>
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "123456789",
+  password: "9808902770",
   database: "PasswordManager",
 });
 
